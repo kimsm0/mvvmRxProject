d542cefbd6e01fe51954f93dd12681c2691d4ba8
kimsoomin <kimsoomin827@naver.com>
2024-01-22 17:30:43 +0900

    @propertyWrapper update

--- MvvmRxProject/MvvmRxProject/PropertyWrapper/UserDefault.swift
+++ MvvmRxProject/MvvmRxProject/PropertyWrapper/UserDefault.swift
@@ -12,9 +12,16 @@ struct UserDefault<T> {
     
     let key: String
     var defaultValue: T
+    var storage: UserDefaults
     
     var wrappedValue: T {
         get { UserDefaults.standard.object(forKey: key) as? T ?? defaultValue }
         set { UserDefaults.standard.setValue(newValue, forKey: key)}
+    }
+    
+    init(key: String, defaultValue: T, storage: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.storage = storage
     }
 }
